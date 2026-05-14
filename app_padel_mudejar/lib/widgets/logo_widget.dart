49e283314645e36484485f43657b32cc3efe4251
isaboura <isaboura@iesch.org>
2026-05-14 15:30:39 +0200

    logos actualizados

--- app_padel_mudejar/lib/widgets/logo_widget.dart
+++ app_padel_mudejar/lib/widgets/logo_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../core/theme.dart';
 
 class LogoWidget extends StatelessWidget {
   final double size;
@@ -7,41 +6,10 @@ class LogoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: size,
-          height: size,
-          decoration: BoxDecoration(
-            color: AppTheme.primary.withOpacity(0.1),
-            borderRadius: BorderRadius.circular(size * 0.25),
-          ),
-          child: Icon(
-            Icons.sports_tennis_rounded,
-            color: AppTheme.primary,
-            size: size * 0.55,
-          ),
-        ),
-        const SizedBox(height: 12),
-        const Text(
-          'CLUB DE PÁDEL',
-          style: TextStyle(
-            fontSize: 13,
-            fontWeight: FontWeight.w700,
-            letterSpacing: 2,
-            color: AppTheme.textMedium,
-          ),
-        ),
-        const Text(
-          'MUDÉJAR',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w800,
-            letterSpacing: 3,
-            color: AppTheme.textDark,
-          ),
-        ),
-      ],
+    return Image.asset(
+      'assets/logoclean.png',
+      width: size,
+      height: size,
     );
   }
 }
